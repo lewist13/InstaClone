afb319e43d6f3c72c9914094a52cccef18af4ad5
lewist13 <lewistpro@gmail.com>
2020-11-15 13:08:30 -0500

    changed package.json to avoid define errors, added connection.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-import express from "express";
-import logger from "morgan";
-import cors from "cors";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import Pusher from "pusher";
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Pusher = require("pusher");
+const connection = require("./db/connection");
 
 // app config
 const PORT = process.env.PORT || 3001;
@@ -23,6 +24,7 @@ app.get("/", (req, res) => res.status(200).send("yoooo it works"));
 // listen
 app.listen(PORT, async () => {
   try {
+    await connection;
     console.log("Database Connected");
     console.log(`App listening on port: ${PORT}`);
   } catch (error) {
