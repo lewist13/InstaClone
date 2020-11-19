cc9f3b8e9c5af2637c876b387e4fdbebe4769858
lewist13 <lewistpro@gmail.com>
2020-11-18 23:39:49 -0500

    updated signup

---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import TextInput from "../components/TextInput";
+import { __RegisterUser } from "../services/UserServices";
+import "../styles/App.css";
+
+export default class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+    console.log(this.state);
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await __RegisterUser(this.state);
+      this.props.history.push("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    const { name, password, email } = this.state;
+    return (
+      <div className="app">
+        <form className="app__login">
+          <center>
+            <img
+              className="app__headerImage"
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+              alt=""
+            />
+          </center>
+          <TextInput
+            type="text"
+            placeholder="username"
+            value={name}
+            onChange={this.handleChange}
+          />
+          <TextInput
+            placeholder="email"
+            type="text"
+            value={email}
+            onChange={this.handleChange}
+          />
+          <TextInput
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+          <button>Register</button>
+        </form>
+      </div>
+    );
+  }
+}
