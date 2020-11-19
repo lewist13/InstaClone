b88555b944c7e924e0fef04f85ff3ef3bc00fe9e
lewist13 <lewistpro@gmail.com>
2020-11-18 22:18:53 -0500

    fixed mongoose connection

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
-const connection = mongoose.connect(
-  process.env.NODE_ENV === "production"
-    ? process.env.DATABASE_URL
-    : "mongodb://localhost:27017/instaclone"
-);
+// const connection = mongoose.connect(
+//   process.env.NODE_ENV === "production"
+//     ? process.env.DATABASE_URL
+//     : "mongodb://localhost:27017/instaclone"
+// );
 
-mongoose.connect(connection, {
+const connection = mongoose.connect("mongodb://localhost:27017/instaclone", {
   useNewUrlParser: true,
   useFindAndModify: true,
   useUnifiedTopology: true,
