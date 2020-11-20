deb2ea0c0af13eac0d3e62010020dd25e2107dcc
lewist13 <lewistpro@gmail.com>
2020-11-19 19:07:43 -0500

    made many changes and added a few pages/components

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -1,46 +1,99 @@
 import React, { Component } from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
-import Update from '../pages/UpdatePost'
-import Post from "./components/Post";
-import ImageUpload from "./components/ImageUpload";
-
+import { __CheckSession } from "../services/UserServices";
+import Home from "../pages/Home";
+import SignIn from "../pages/SignIn";
+import ProtectedRoute from "./ProtectedRoute";
+import Layout from "../components/Layout";
+import Profile from "../pages/Profile";
+import UpdatePost from "../pages/UpdatePost";
+// import Update from "../pages/UpdatePost";
+// import Post from "./components/Post";
+// import ImageUpload from "./components/ImageUpload";
 class Router extends Component {
-    constructor() {
-        super()
-    }
-}
-
-componentDidMount() {
-    this.verifyTokenValid()
-    this.setState({pageLoading: false})
-}
-
-verifyTokenValid = async () => {
-    const token = localStorage.getItem('token')
+  constructor() {
+    super();
+    this.state = {
+      authenticated: false,
+      currentUser: null,
+      pageLoading: true,
+    };
+  }
+  componentDidMount() {
+    this.verifyTokenValid();
+    this.setState({ pageLoading: false });
+  }
+  verifyTokenValid = async () => {
+    const token = localStorage.getItem("token");
     if (token) {
-        try {
-            const session = await __CheckSession()
-            console.log('session', session)
-            this.setState(
-                {
-                    currentUser: session.user,
-                    authenticated: true
-                },
-                () => this.props.history.push('/profile')
-            )
-        } catch (error) {
-            this.setState({ currentUser: null, authenticated: false })
-            localStorage.clear()
-        }
+      try {
+        const session = await __CheckSession();
+        console.log("session", session);
+        this.setState(
+          {
+            currentUser: session.user,
+            authenticated: true,
+          },
+          () => this.props.history.push("/profile")
+        );
+      } catch (error) {
+        this.setState({ currentUser: null, authenticated: false });
+        localStorage.clear();
+      }
     }
-}
-  
-toggleAuthenticated = (value, user, done) => {
-    this.setState({ authenticated: value, currentUser: user }, () => done())
-  }
-
-render() {
+  };
+  toggleAuthenticated = (value, user, done) => {
+    this.setState({ authenticated: value, currentUser: user }, () => done());
+  };
+  render() {
     return (
-        <ma
-    )
+      // home = '/', signup = '/register', signin = '/login', update = '/edit/:post_id, create = '/upload'
+      <main>
+        {this.state.pageLoading ? (
+          <h3>Loading...</h3>
+        ) : (
+          <Switch>
+            <Route exact path="/" component={() => <Home />} />
+            <Route
+              path="/login"
+              component={(props) => (
+                <Home>
+                  <SignIn
+                    toggleAuthenticated={this.toggleAuthenticated}
+                    {...props}
+                  />
+                </Home>
+              )}
+            />
+            <ProtectedRoute
+              authenticated={this.state.authenticated}
+              path="/profile"
+              component={(props) => (
+                <Layout
+                  currentUser={this.state.currentUser}
+                  authenticated={this.state.authenticated}
+                >
+                  <Profile {...props} currentUser={this.state.currentUser} />
+                </Layout>
+              )}
+            />
+            <ProtectedRoute
+              authenticated={this.state.authenticated}
+              path="/edit/:post_id"
+              component={(props) => (
+                <Layout
+                  currentUser={this.state.currentUser}
+                  authenticated={this.state.authenticated}
+                >
+                  <UpdatePost {...props} currentUser={this.state.currentUser} />
+                </Layout>
+              )}
+            />
+          </Switch>
+        )}
+      </main>
+    );
   }
+}
+
+export default withRouter(Router);
---
+++ client/src/pages/Profile.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import { __DeletePost } from "../services/PostServices";
+import { __GetProfile } from "../services/UserServices";
+
+export default class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      postFetchError: false,
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getPosts();
+  }
+
+  getPosts = async () => {
+    try {
+      console.log(this.props);
+      const profileData = await __GetProfile(this.props.currentUser._id);
+      this.setState({ posts: profileData.posts });
+    } catch (error) {
+      this.setState({ postFetchError: true });
+    }
+  };
+
+  deletePost = async (id) => {
+    try {
+      const postsToKeep = this.state.posts.filter((post) => post._id !== id);
+      this.setState({ posts: postsToKeep });
+      await __DeletePost(id);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    return (
+      <div className="profile">
+        <div>
+          {this.state.posts.length ? (
+            <div className="post-content wrapper flex-row">
+              {this.state.posts.map((post) => (
+                <div key={post._id}>
+                  <div className="flex-row button-wrapper">
+                    <button
+                      onClick={() =>
+                        this.props.history.push(`/edit/${post._id}`)
+                      }
+                    >
+                      Edit
+                    </button>
+                    <button onClick={() => this.deletePost(post._id)}>
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="span message">No Posts</div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
